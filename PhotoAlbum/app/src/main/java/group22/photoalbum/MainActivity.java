912d6de6cfca8bb87d2461b7c6b3652091490072
Ramzy Saleh <rs1064@scarletmail.rutgers.edu>
2016-04-24 00:17:31 -0400

    Add albums!

--- PhotoAlbum/app/src/main/java/group22/photoalbum/MainActivity.java
+++ PhotoAlbum/app/src/main/java/group22/photoalbum/MainActivity.java
@@ -6,10 +6,25 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.AlertDialog;
+import android.widget.EditText;
+import android.text.InputType;
+import android.content.DialogInterface;
+import android.content.Context;
+import java.util.ArrayList;
+import group22.photoalbum.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    final Context context = this;
+    String userResponse;
+    static ArrayList<Album> albums = new ArrayList<Album>();
+    ListView albumNames;
+    ArrayAdapter<Album> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +33,39 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        albumNames = (ListView)findViewById(R.id.photo_list);
+
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Enter album name.");
+
+                final EditText input = new EditText(context);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        userResponse = input.getText().toString();
+                        Album albumToAdd = new Album(userResponse);
+                        albums.add(albumToAdd);
+                        arrayAdapter = new ArrayAdapter<Album>(context,R.layout.album,albums);
+                        albumNames.setAdapter(arrayAdapter);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+
             }
         });
     }
